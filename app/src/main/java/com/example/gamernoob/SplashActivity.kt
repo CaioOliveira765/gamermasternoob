fbd2ae34735163f1dcbb7628aa8422a0faf0fd4f
Caio Oliveira Santos <caiosantos765@gmail.com>
2019-03-18 17:34:10 -0300

    #03 - Tela de Login nao Finalizada

--- app/src/main/java/com/example/gamernoob/SplashActivity.kt
+++ app/src/main/java/com/example/gamernoob/SplashActivity.kt
@@ -1,13 +1,41 @@
 package com.example.gamernoob
 
+
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import com.example.gamernoob.view.LoginActivity
+
 
 class SplashActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    var mDelayhandler : Handler? = null
+    val SPLASH_DELAY : Long = 2500
+
+         val mRunnable : Runnable = Runnable {
+        if (!isFinishing){
+            val intent = Intent(applicationContext, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+        override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
+            mDelayhandler = Handler()
+
+            mDelayhandler!!.postDelayed(mRunnable, SPLASH_DELAY)
+
+    }
+
+    public override fun onDestroy() {
+        if (mDelayhandler != null){
+            mDelayhandler!!.removeCallbacks(mRunnable)
+        }
+
+        super.onDestroy()
     }
 }
--- app/src/main/java/com/example/gamernoob/firebasedata/FirebaseConstants.kt
+++
@@ -1,9 +0,0 @@
-package com.example.gamernoob.firebasedata
-
-object FirebaseConstants {
-    val COLLECTION_PARENT = "appQuotes"
-    val DOCUMENT_PARENT = "quotes"
-
-    val FIELD_CONTENT="content"
-    val FIELD_CONTENT_TYPE="contentType"
-}
