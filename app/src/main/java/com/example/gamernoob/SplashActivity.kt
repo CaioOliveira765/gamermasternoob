bfcf1619a75bf570cd519651ed683d7e6e7eae48
Caio Oliveira Santos <caiosantos765@gmail.com>
2019-03-21 17:17:09 -0300

    #04 - tecla Facebook com erro

--- app/src/main/java/com/example/gamernoob/SplashActivity.kt
+++ app/src/main/java/com/example/gamernoob/SplashActivity.kt
@@ -5,7 +5,7 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
-import com.example.gamernoob.view.LoginActivity
+import com.exemples.gamernoob.view.LoginActivity
 
 
 class SplashActivity : AppCompatActivity() {
@@ -15,9 +15,8 @@ class SplashActivity : AppCompatActivity() {
 
          val mRunnable : Runnable = Runnable {
         if (!isFinishing){
-            val intent = Intent(applicationContext, LoginActivity::class.java)
+            val intent = Intent(this@SplashActivity, LoginActivity::class.java)
             startActivity(intent)
-            finish()
         }
     }
 
--- app/src/main/java/com/example/gamernoob/view/LoginActivity.kt
+++ app/src/main/java/com/example/gamernoob/view/LoginActivity.kt
@@ -1,39 +1,107 @@
-package com.example.gamernoob.view
+package  com.exemples.gamernoob.view
 
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.telecom.Call
+import android.util.Log
+import android.view.View
 import com.example.gamernoob.R
-import com.example.gamernoob.Retrofit.RetrofitConfig
-import com.example.gamernoob.model.Usuario
+import com.example.gamernoob.view.PlaylistActivity
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.api.GoogleApiClient
 import kotlinx.android.synthetic.main.activity_login.*
-import okhttp3.Callback
-import okhttp3.ResponseBody
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
+import java.util.*
+import javax.security.auth.AuthPermission
 
-class LoginActivity : AppCompatActivity() {
+
+class LoginActivity : AppCompatActivity(), GoogleApiClient.OnConnectionFailedListener {
+    override fun onConnectionFailed(connectionResult : ConnectionResult) {
+    Log.d("bett", "onConnectionFailed:" + connectionResult)
+    }
+
+    private val RC_SIGN_IN = 9001
+    private var mGoogleApiCliente: GoogleApiClient? = null
+    var callbackMAnager: CallbackManager? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        val call = RetrofitConfig().loginService().buscarLogin()
+        FacebookSdk.sdkInitialize(applicationContext)
+        AppEventsLogger.activateApp(this)
+
+        btnLoginFacebook.setOnClickListener(View.OnClickListener {
+            callbackMAnager = CallbackManager.Factory.create()
+            LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("public profile", "email"))
+            LoginManager.getInstance().registerCallback(callbackMAnager,
+                object  : FacebookCallback<LoginResult>{
+                    override fun onSuccess(loginResult: LoginResult?) {
+                            Log.d("LoginActivity", "Facebook token: " +  loginResult?.accessToken?.token)
+                            startActivity(Intent(applicationContext, PlaylistActivity::class.java))
+                    }
+
+                    override fun onCancel() {
+                        Log.d("LoginActivity", "Facebook cancel.")
+                    }
+
+                    override fun onError(error: FacebookException?) {
+                        Log.d("LoginActivity", "Facebook cancel.")
+                    }
+                }
+        })
 
 
-        btnLogin.setOnClickListener() {
 
-            val emailDigitado = email.getText().toString()
-            val senhadigitada = senha.getText().toString()
-            val usuarioLista = Usuario(emailDigitado, senhadigitada)
 
-            var intent = Intent(this@LoginActivity, PlaylistActivity::class.java )
-            startActivity(intent)
 
+
+
+
+        updateUI(false)
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build()
+
+        mGoogleApiCliente = GoogleApiClient.Builder(this)
+            .enableAutoManage(this, this)
+            .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+            .build()
+
+        loginGoogle?.setOnClickListener(View.OnClickListener {
+            var googleIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiCliente)
+            startActivityForResult(googleIntent, RC_SIGN_IN)
+
+            loginGoogle.setOnClickListener(){
+                val intent = Intent(this@LoginActivity, PlaylistActivity::class.java)
+                startActivity(intent)
+            }
+        })
+        }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RC_SIGN_IN){
+            var result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
+            updateUI(result.isSuccess)
         }
 
     }
 
-    private fun Usuario(senha: String, nome: String): Usuario {
-
+    private fun updateUI(isLogin: Boolean) {
+        if (isLogin){
+        }
     }
+
 }
+
